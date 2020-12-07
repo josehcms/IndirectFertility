aa075ff160af4b7dd427909eb36b502c5af2f988
José H C Monteiro da Silva <zecosta.monteiro@gmail.com>
2020-12-07 10:36:14 -0300

    update ceb results with and without shift

--- R/2_CEBFertilityEstimation.R
+++ R/2_CEBFertilityEstimation.R
@@ -2,7 +2,7 @@
 ### Title: Children Ever Born Estimation using
 ###        Demodata
 ### Author: Jose H C Monteiro da Silva
-### Last Update: 2020-12-04
+### Last Update: 2020-12-07
 ##################################################
 
 ### Set up packages and global options #----------
@@ -48,11 +48,15 @@ ceb_data <-
                       '60-64', '65-69', '70-74', '75-79' ) ] %>%
   setorder( SeriesID, LocID, AgeStart )
 
+# select series ids 
 ids <- 
   ceb_data$SeriesID %>% 
   unique
 
-ceb_list <- 
+##################################################
+
+### Prepare mean ceb dataset with age shifts #----
+ceb_list_shift <- 
   lapply( ids, function( x ){
     
     print( x )
@@ -112,41 +116,128 @@ ceb_list <-
     return( res )
   } )
 
-ceb_res <- 
-  do.call( rbind, ceb_list )
+ceb_res_shift <- 
+  do.call( rbind, ceb_list_shift )
 
-out_ceb <- 
+out_ceb_shift <- 
   merge(
     ceb_data[,
-             .( SeriesID, LocID, LocName, LocTypeName, LocAreaTypeName, ## series ID charactr
-                SubGroupName,  SubGroupTypeName, SubGroupCombinationID, 
+             .( SeriesID, LocID, LocName, LocTypeName, 
+                LocAreaTypeName, SubGroupName,  
+                SubGroupTypeName, SubGroupCombinationID,
                 DataCatalogID, DataCatalogName, DataCatalogShortName, 
-                FieldWorkStart, FieldWorkMiddle, DataProcess, DataSourceName, 
-                DataSourceAuthor, DataSourceYear, DataSourceShortName, 
-                DataStatusName, StatisticalConceptName, DataTypeName, 
-                ModelPatternName, DataReliabilityName, PeriodTypeName, 
-                PeriodGroupName, TimeUnit, FootNoteID,
-                RegName, RegID, AreaName, AreaID, 
+                FieldWorkStart, FieldWorkMiddle, DataProcess, 
+                DataSourceName, DataSourceAuthor, DataSourceYear, 
+                DataSourceShortName, DataStatusName, 
+                StatisticalConceptName, DataTypeName, ModelPatternName, 
+                DataReliabilityName, PeriodTypeName, PeriodGroupName, 
+                TimeUnit, FootNoteID, RegName, RegID, AreaName, AreaID, 
                 TimeStart, TimeEnd, TimeMid, TimeLabel,                    
                 SexName, SexID ) ] %>% unique,
-    ceb_res[ ,.( SeriesID, 
-                 DateFormtd = as.Date( date_decimal( year_ceb ) ),
-                 year = year_ceb,
-                 ages_w,
-                 mac,
-                 CEB = CFR,
-                 TypeEst = 'CEB' ) ],
+    ceb_res_shift[ ,
+                   .( SeriesID,
+                      DateFormtd = as.Date( date_decimal( year_ceb ) ),
+                      year = year_ceb,
+                      ages_w,
+                      mac,
+                      CEB = CFR,
+                      TypeEst = 'CEB-Age Shift' ) ],
+    by = 'SeriesID'
+    ) %>%
+  setorder( LocID, TimeMid, SeriesID, ages_w )
+
+write.table( out_ceb_shift, 
+             'outputs/ceb_fertest_world_shift.csv',
+             row.names = F )
+##################################################
+
+### Prepare mean ceb dataset without age shifts #----
+ceb_list_noshift <- 
+  lapply( ids, function( x ){
+    
+    print( x )
+    
+    aux <- 
+      ceb_data[ SeriesID == x ] %>% copy %>%
+      setorder( AgeStart )
+    
+    locid    <- aux$LocID %>% unique
+    locname  <- aux$LocName %>% unique
+    
+    mean_ceb <- aux$DataValue
+    ages_w   <- aux$AgeStart
+    age_interval <- aux$AgeEnd - aux$AgeStart
+    midgroup_ages <- ( ages_w ) + age_interval / 2
+    
+    year_svy <- 
+      aux$TimeMid %>% 
+      unique 
+    
+    # get mean age of childbearing based on 
+    # svy year ASFR distribution
+    mac <- 
+      rep(
+        fertestr::fetch_mac_Wpp2019( location_code = locid,
+                                     year = year_svy ),
+        length( ages_w )
+      )
+    
+    year_ceb <- round( year_svy - ( midgroup_ages - mac ), 3 )
+    
+    res <- 
+      data.table(
+        SeriesID = x,
+        LocID    = locid,
+        LocName  = locname,
+        year_svy = year_svy,
+        ages_w = ages_w,
+        year_ceb = c( year_ceb ),
+        mac  = c( mac ),
+        CFR  = mean_ceb,
+        TypeEst = 'CEB' ) 
+    
+    return( res )
+  } )
+
+ceb_res_noshift <- 
+  do.call( rbind, ceb_list_noshift )
+
+out_ceb_noshift <- 
+  merge(
+    ceb_data[,
+             .( SeriesID, LocID, LocName, LocTypeName, 
+                LocAreaTypeName, SubGroupName,  
+                SubGroupTypeName, SubGroupCombinationID,
+                DataCatalogID, DataCatalogName, DataCatalogShortName, 
+                FieldWorkStart, FieldWorkMiddle, DataProcess, 
+                DataSourceName, DataSourceAuthor, DataSourceYear, 
+                DataSourceShortName, DataStatusName, 
+                StatisticalConceptName, DataTypeName, ModelPatternName, 
+                DataReliabilityName, PeriodTypeName, PeriodGroupName, 
+                TimeUnit, FootNoteID, RegName, RegID, AreaName, AreaID, 
+                TimeStart, TimeEnd, TimeMid, TimeLabel,                    
+                SexName, SexID ) ] %>% unique,
+    ceb_res_noshift[ ,
+                   .( SeriesID,
+                      DateFormtd = as.Date( date_decimal( year_ceb ) ),
+                      year = year_ceb,
+                      ages_w,
+                      mac,
+                      CEB = CFR,
+                      TypeEst = 'CEB-No Shift' ) ],
     by = 'SeriesID'
   ) %>%
   setorder( LocID, TimeMid, SeriesID, ages_w )
 
-write.table( out_ceb, 
-             'outputs/ceb_fertest_world.csv',
+write.table( out_ceb_noshift, 
+             'outputs/ceb_fertest_world_noshift.csv',
              row.names = F )
+##################################################
 
+### Plot Results #--------------------------------
 data('tfr')
 
-myLocations <- ceb_res$LocID %>% unique
+myLocations <- ceb_res_shift$LocID %>% unique
 
 # get wpp 2019 fertility data and compute reference period
 wpp_tfr <- 
@@ -166,28 +257,38 @@ wpp_tfr <-
 plot_all <- 
   rbind(
     wpp_tfr[, .( SeriesID = 0, 
-                 LocID, name, year, TFR, 
+                 LocID, 
+                 name, 
+                 year, 
+                 TFR, 
                  TypeEst = 'WPP 2019' ) ],
-    ceb_res[, .( SeriesID, LocID, name = LocName, 
-                 year = year_ceb, TFR = CFR, TypeEst ) ]
+    ceb_res_noshift[, 
+                    .( SeriesID, 
+                       LocID, 
+                       name = LocName,
+                       year = year_ceb, 
+                       TFR = CFR, 
+                       TypeEst = 'CEB-No Shift' ) ],
+    ceb_res_shift[, 
+                  .( SeriesID, 
+                     LocID, 
+                     name = LocName,
+                     year = year_ceb, 
+                     TFR = CFR, 
+                     TypeEst = 'CEB-Age Shift' ) ]
   )
 
 pdf( file = 'figs/outputs_ceb_world.pdf', 
      width = 8, height = 6 )
 for( loc in sort( plot_all$LocID %>% unique ) ){
   
-  aux <- plot_all[ LocID == loc & TypeEst %in% c( 'WPP 2019', 
-                                                  'CEB' ) ]
+  aux <- 
+    plot_all[ LocID == loc, ] %>% copy
   
-  title_name <- paste0( aux$name %>% unique, ' - ', loc )
-  
-  if( max( aux$TFR ) > 15 | min( aux$TFR ) < 0.5 ){
-    filterOutLier <- 
-      aux[ TFR > 15 | TFR < 0.5, ]$SeriesID %>%
-      unique
-    
-    aux <- aux[ !( SeriesID %in% filterOutLier ) ]
-  }
+  title_name <- 
+    paste0( aux$name %>% unique, 
+            ' - ', 
+            loc )
   
   plot_this <- 
     ggplot() +
@@ -206,20 +307,20 @@ for( loc in sort( plot_all$LocID %>% unique ) ){
     scale_x_continuous( limits = c( 1930, 2020 ), 
                         breaks = seq( 1930, 2030, 10 ) ) +
     scale_color_manual( values = c( 'Abridged-BeersModified' = 'forestgreen',
-                                    'CEB' = 'steelblue3',
-                                    'UNESCO-WPP'  = 'black',
+                                    'CEB-Age Shift' = 'steelblue3',
+                                    'CEB-No Shift'  = 'black',
                                     'UNESCO-Demodata'  = 'gray55',
                                     'WPP 2019' = 'tomato3'),
                         name = '' ) +
     scale_shape_manual( values = c( 'Abridged-BeersModified' = 4,
-                                    'CEB' = 19,
-                                    'UNESCO-WPP'  = 18,
+                                    'CEB-Age Shift' = 19,
+                                    'CEB-No Shift'  = 15,
                                     'UNESCO-Demodata'   = 16,
                                     'WPP 2019' = 20 ),
                         name = '' ) +
     scale_linetype_manual( values = c( 'Abridged-BeersModified' = 'blank',
-                                       'CEB' = 'blank',
-                                       'UNESCO-WPP'  = 'blank',
+                                       'CEB-Age Shift' = 'blank',
+                                       'CEB-No Shift'  = 'blank',
                                        'UNESCO-Demodata'  = 'blank',
                                        'WPP 2019' = 'solid' ),
                            name = '' ) +
